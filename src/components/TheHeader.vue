cc620462fc8bf8c4b92b15c4021eee6c6f71cc4f
Fabian Wassermann <fabian.wassermann04@gmail.com>
2023-02-09 12:33:26 +0100

    simple main view

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="px-6 pt-6 lg:px-8 max-w-[90rem] mx-auto">
+    <nav class="flex items-center justify-between" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <!-- <img
+            class="h-8"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt=""
+          /> -->
+          <span
+            class="
+              font-bold
+              text-3xl
+              tracking-wide
+              text-gray-200
+              uppercase
+              mix-blend-difference
+            "
+            >Labrador</span
+          >
+        </a>
+      </div>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="
+            -m-2.5
+            inline-flex
+            items-center
+            justify-center
+            rounded-md
+            p-2.5
+            text-gray-200
+            mix-blend-difference
+          "
+          @click="mobileMenuOpen = true"
+        >
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+        <a
+          v-for="item in navigation"
+          :key="item.name"
+          :href="item.href"
+          class="
+            text-md
+            font-medium
+            leading-6
+            text-gray-200
+            mix-blend-difference
+          "
+          >{{ item.name }}</a
+        >
+      </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a
+          href="#"
+          class="
+            text-md
+            font-semibold
+            leading-6
+            text-gray-200
+            mix-blend-difference
+          "
+          >Kontakt <span aria-hidden="true">&rarr;</span></a
+        >
+      </div>
+    </nav>
+    <Dialog as="div" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <DialogPanel
+        focus="true"
+        class="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden"
+      >
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img
+              class="h-8"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+          <button
+            type="button"
+            class="-m-2.5 rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = false"
+          >
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                class="
+                  -mx-3
+                  block
+                  rounded-lg
+                  py-2
+                  px-3
+                  text-base
+                  font-semibold
+                  leading-7
+                  text-gray-900
+                  hover:bg-gray-400/10
+                "
+                >{{ item.name }}</a
+              >
+            </div>
+            <div class="py-6">
+              <a
+                href="#"
+                class="
+                  -mx-3
+                  block
+                  rounded-lg
+                  py-2.5
+                  px-3
+                  text-base
+                  font-semibold
+                  leading-6
+                  text-gray-900
+                  hover:bg-gray-400/10
+                "
+                >Kontakt</a
+              >
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+  { name: "Über uns", href: "#" },
+  { name: "Neuigkeiten", href: "#" },
+  { name: "Wurfplanung", href: "#" },
+  { name: "Galerie", href: "#" },
+];
+
+const mobileMenuOpen = ref(false);
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,41 @@
-<template></template>
+This example requires Tailwind CSS v3.0+
+<template>
+  <!-- after:fixed
+      after:top-0
+      after:bottom-0
+      after:left-0
+      after:right-0
+      after:bg-black/10 -->
+  <div
+    class="isolate bg-white relative aspect-video bg-cover"
+    style="
+      /* background-image: url('https://images.unsplash.com/photo-1618153807812-a12a1950d93c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'); */
+      background-image: url('https://images.unsplash.com/photo-1524401033441-e87cab019093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
+    "
+  >
+    <TheHeader class="w-full"></TheHeader>
+    <main class="px-6 pt-6 lg:px-8 max-w-[90rem] mx-auto h-full">
+      <div class="h-2/3 flex flex-col justify-center">
+        <h1
+          class="
+            w-fit
+            text-gray-200 text-8xl
+            tracking-wide
+            font-bold
+            leading-[1.15]
+            mix-blend-difference
+          "
+        >
+          Dein <br />bester Freund.
+        </h1>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+import TheHeader from "@/components/TheHeader.vue";
+</script>
