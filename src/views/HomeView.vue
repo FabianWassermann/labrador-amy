20f8c707fe33a996d9feffe1576c907151967624
Fabian Wassermann <fabian.wassermann04@gmail.com>
2023-02-10 09:12:34 +0100

    animate images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,12 +16,23 @@ This example requires Tailwind CSS v3.0+
       min-h-[40rem]
       h-screen
       flex flex-col
-    "
-    style="
-      /* background-image: url('https://images.unsplash.com/photo-1618153807812-a12a1950d93c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'); */
-      background-image: url('https://images.unsplash.com/photo-1524401033441-e87cab019093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
+      after:fixed
+      after:top-0
+      after:bottom-0
+      after:left-0
+      after:right-0
+      after:bg-black/10
+      after:pointer-events-none
     "
   >
+    <!-- Image Slider -->
+    <div
+      v-for="(image, index) of images"
+      :key="index"
+      ref="imageRefs"
+      :style="`background-image: url('${image.src}');`"
+      class="h-full w-full absolute hidden"
+    ></div>
     <!-- Backdrop -->
     <div
       class="
@@ -56,7 +67,8 @@ This example requires Tailwind CSS v3.0+
         <h1
           class="
             w-fit
-            text-gray-900 text-9xl
+            text-gray-400 text-8xl
+            2xl:text-9xl
             tracking-wide
             font-bold
             leading-[1.15]
@@ -70,18 +82,17 @@ This example requires Tailwind CSS v3.0+
         class="ml-auto bg-gray-900/75 p-8 text-gray-100 max-w-[18rem] w-full"
       >
         <p class="font-bold text-5xl inline-flex items-start">
-          01 <span class="text-lg font-medium mt-1 ml-1">/ 04</span>
+          01
+          <span class="text-lg font-medium mt-1 ml-1"
+            >/ {{ String(images.length).padStart(2, "0") }}</span
+          >
         </p>
-        <div
-          class="
-            relative
-            w-full
-            h-1
-            bg-gray-400
-            my-2
-            before:h-full before:absolute before:w-1/2 before:bg-gray-100
-          "
-        ></div>
+        <div class="relative w-full h-1 bg-gray-400 my-2">
+          <div
+            class="absolute top-0 left-0 bg-gray-100 h-full"
+            ref="slideProgress"
+          ></div>
+        </div>
         <p class="font-semibold text-lg">Amy</p>
         <p class="leading-5 text-lg mt-2 font-light">
           Sie liebt es in Wiesen zu sitzen.
@@ -92,8 +103,68 @@ This example requires Tailwind CSS v3.0+
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, watch } from "vue";
 import { Dialog, DialogPanel } from "@headlessui/vue";
 import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
 import TheHeader from "@/components/TheHeader.vue";
+
+onMounted(() => {
+  imageRefs.value[currImageIndex.value].style.display = "block";
+  slideProgress.value.style.width = `${
+    ((currImageIndex.value + 1) / images.length) * 100
+  }%`;
+  setInterval(() => {
+    currImageIndex.value =
+      currImageIndex.value === images.length - 1 ? 0 : currImageIndex.value + 1;
+  }, 10000);
+});
+
+let currImageIndex = ref(0);
+const imageRefs = ref([]);
+const slideProgress = ref(null);
+
+watch(currImageIndex, (newIndex, oldIndex) => {
+  const currRef = imageRefs.value[newIndex];
+  const oldRef = imageRefs.value[oldIndex];
+  doImageSliderChanges(currRef, oldRef);
+});
+
+function doImageSliderChanges(currRef, oldRef) {
+  currRef.style.display = "block";
+  currRef.style.zIndex = "-10";
+
+  oldRef.style.zIndex = "-11";
+
+  const imageSlideDuration = 750;
+
+  slideProgress.value.animate(
+    [{ width: `${((currImageIndex.value + 1) / images.length) * 100}%` }],
+    {
+      duration: imageSlideDuration,
+      fill: "forwards",
+      easing: "ease-out",
+    }
+  );
+
+  currRef.animate(
+    [{ transform: "translateX(-100%)" }, { transform: "translateX(0)" }],
+    {
+      duration: imageSlideDuration,
+      easing: "ease-out",
+    }
+  );
+
+  setTimeout(() => {
+    oldRef.style.display = "none";
+  }, imageSlideDuration);
+}
+
+const images = [
+  {
+    src: "https://images.unsplash.com/photo-1524401033441-e87cab019093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1618153807812-a12a1950d93c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  },
+];
 </script>
