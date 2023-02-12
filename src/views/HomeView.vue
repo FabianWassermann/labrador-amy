220fab5343f09e904c27ac03059a591d71c37307
Fabian Wassermann <fabian.wassermann04@gmail.com>
2023-02-12 23:12:41 +0100

    calm image index transition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -82,10 +82,12 @@ This example requires Tailwind CSS v3.0+
         class="ml-auto bg-gray-900/75 p-8 text-gray-100 max-w-[18rem] w-full"
       >
         <div class="inline-flex items-start">
-          <div class="relative flex overflow-hidden items-center h-12 numbers">
-            01
-          </div>
-          <span class="text-lg font-medium mt-1" ref="slideNumber"
+          <transition>
+            <span :key="currImageIndex" class="text-5xl font-bold">
+              {{ String(currImageIndex + 1).padStart(2, "0") }}
+            </span>
+          </transition>
+          <span class="text-lg font-medium mt-1 ml-1"
             >/ {{ String(images.length).padStart(2, "0") }}</span
           >
         </div>
@@ -104,30 +106,6 @@ This example requires Tailwind CSS v3.0+
   </div>
 </template>
 
-<style>
-.numbers {
-  font-size: 3rem;
-  line-height: 1;
-  font-weight: 700;
-}
-.numbers span {
-  z-index: 1;
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  transition: all 2s ease;
-  transform: translateY(0);
-  line-height: 1;
-}
-.numbers span span {
-  flex: 0 0 100%;
-  height: 100%;
-  font-size: 3rem;
-  line-height: 1;
-  font-weight: 700;
-}
-</style>
-
 <script setup>
 import { ref, onMounted, watch } from "vue";
 import { Dialog, DialogPanel } from "@headlessui/vue";
@@ -139,109 +117,22 @@ onMounted(() => {
   slideProgress.value.style.width = `${
     ((currImageIndex.value + 1) / images.length) * 100
   }%`;
-  slideNumber.value.style.marginLeft = "4px";
-  oldSlideNumbers.value = createNumberArray(currImageIndex.value + 1);
 
   setInterval(() => {
     currImageIndex.value =
       currImageIndex.value === images.length - 1 ? 0 : currImageIndex.value + 1;
-  }, 10000);
+  }, 5000);
 });
 
 let currImageIndex = ref(0);
 const imageRefs = ref([]);
 const slideProgress = ref(null);
-const slideNumber = ref(null);
-let oldSlideNumbers = ref([]);
 
 watch(currImageIndex, (newIndex, oldIndex) => {
   const currRef = imageRefs.value[newIndex];
   const oldRef = imageRefs.value[oldIndex];
   doImageSliderChanges(currRef, oldRef);
-  animateNumber(currImageIndex.value + 1, document.querySelector(".numbers"));
-  doSlideNumberChanges();
 });
-
-function doSlideNumberChanges() {
-  if (currImageIndex.value + 1 >= 3) return;
-  const marginLeft = currImageIndex.value + 1 === 1 ? "-4px" : "4px";
-  slideNumber.value.animate([{ marginLeft: marginLeft }], {
-    duration: 1000,
-    fill: "forwards",
-    easing: "ease-out",
-  });
-}
-
-function createNumberArray(number) {
-  // Zerlegt die Nummer in einen Array
-  const numberArray = number.toString().split("");
-  for (let i = 0; numberArray.length < 2; i++) {
-    numberArray.unshift(0);
-  }
-  // gibt den Array zurück
-  return numberArray.map((x) => parseInt(x));
-}
-
-function animateNumber(number, element) {
-  // Leert das Element
-  element.innerHTML = "";
-  // Berechnet den neuen Number Array
-  const numberArray = createNumberArray(number);
-  // Legt alles in das HTML Element
-  createNumberHTML(numberArray, oldSlideNumbers.value, element);
-  // Berechnet die Ticks die verändert werden sollen.
-  const ticks = [...element.querySelectorAll("span[data-value]")];
-  setTimeout(() => {
-    // Animiert die Werte
-    for (let tick of ticks) {
-      let dist = parseInt(tick.getAttribute("data-value") - 1);
-      tick.style.transform = `translateY(-${dist * 100}%)`;
-    }
-  }, 0);
-  // Setzten den Zahlen Array zu dem Alten Status
-  oldSlideNumbers.value = numberArray;
-}
-
-function createNumberHTML(numbers, old, element) {
-  for (let i = 0; i < numbers.length; i++) {
-    if (isNaN(numbers[i]) || isNaN(old[i])) {
-      element.insertAdjacentHTML(
-        "beforeend",
-        `<span data-value="${
-          calcDeltaSight(old[i], numbers[i]).length
-        }">${calcDeltaSight(old[i], numbers[i]).join("")}</span>`
-      );
-    } else {
-      element.insertAdjacentHTML(
-        "beforeend",
-        `<span data-value="${
-          calcDeltaBetweenNumbers(old[i], numbers[i]).length
-        }">${calcDeltaBetweenNumbers(old[i], numbers[i]).join("")}</span>`
-      );
-    }
-  }
-  return element;
-}
-
-function calcDeltaSight(oldSight, newSight) {
-  return oldSight !== newSight
-    ? [`<span>${oldSight}</span>`, `<span>${newSight}</span>`]
-    : [`<span>${newSight}</span>`];
-}
-
-function calcDeltaBetweenNumbers(oldNumber, newNumber) {
-  let numberArray = [oldNumber];
-  let notFound = true;
-  if (oldNumber === newNumber)
-    return numberArray.map((x) => `<span>${x}</span>`);
-  while (notFound) {
-    oldNumber++;
-    if (oldNumber > 9) oldNumber = 0;
-    numberArray.push(oldNumber);
-    if (oldNumber === newNumber) notFound = false;
-  }
-  return numberArray.map((x) => `<span>${x}</span>`);
-}
 
 function doImageSliderChanges(currRef, oldRef) {
   currRef.style.display = "block";
