dae946f1160c95b1adfd255228fee126a9edb71d
Fabian Wassermann <fabian.wassermann04@gmail.com>
2023-02-14 22:57:30 +0100

    link to home

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,7 +3,7 @@
     <!-- Header -->
     <nav class="flex items-center justify-between" aria-label="Global">
       <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
+        <router-link to="/" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
           <!-- <img
             class="h-8"
@@ -15,7 +15,7 @@
             :class="{ '!text-black': background === 'light' }"
             >Labrador</span
           >
-        </a>
+        </router-link>
       </div>
       <div class="flex lg:hidden">
         <button
